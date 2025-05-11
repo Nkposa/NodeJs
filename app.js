d1837d50de54a8dda8baa9f2a9083a801ac5efcd
naveen <naveenhima91@gmail.com>
2025-05-11 17:36:44 +0530

    added env

--- app.js
+++ app.js
@@ -3,30 +3,31 @@ const express = require('express');
 const OS = require('os');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const cors = require('cors');
+
 const app = express();
-const cors = require('cors')
 
-
+// Middleware
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
-app.use(cors())
+app.use(cors());
 
+// MongoDB Connection using full URI
 mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
     if (err) {
-        console.log("error!! " + err)
+        console.error("MongoDB connection error: " + err);
     } else {
-      //  console.log("MongoDB Connection Successful")
+        console.log("MongoDB Connection Successful");
     }
-})
+});
 
-var Schema = mongoose.Schema;
+// Mongoose Schema and Model
+const Schema = mongoose.Schema;
 
-var dataSchema = new Schema({
+const dataSchema = new Schema({
     name: String,
     id: Number,
     description: String,
@@ -34,54 +35,45 @@ var dataSchema = new Schema({
     velocity: String,
     distance: String
 });
-var planetModel = mongoose.model('planets', dataSchema);
 
+const planetModel = mongoose.model('planets', dataSchema);
 
-
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
+// Routes
+app.post('/planet', function(req, res) {
+    planetModel.findOne({ id: req.body.id }, function(err, planetData) {
+        if (err || !planetData) {
+            res.status(404).send("Planet not found or error occurred.");
         } else {
             res.send(planetData);
         }
-    })
-})
+    });
+});
 
-app.get('/',   async (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
-
-app.get('/os',   function(req, res) {
+app.get('/os', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send({
-        "os": OS.hostname(),
-        "env": process.env.NODE_ENV
+        os: OS.hostname(),
+        env: process.env.NODE_ENV
     });
-})
+});
 
-app.get('/live',   function(req, res) {
+app.get('/live', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "live"
-    });
-})
+    res.send({ status: "live" });
+});
 
-app.get('/ready',   function(req, res) {
+app.get('/ready', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "ready"
-    });
-})
+    res.send({ status: "ready" });
+});
 
+// Start Server
 app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
-})
-
+    console.log("Server successfully running on port - 3000");
+});
 
 module.exports = app;
